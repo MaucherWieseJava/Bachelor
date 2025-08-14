78837eeb28c07ed457085423fd340724e05c9a9c
Adrian Alois Stötzler <adrian@stoetzler.de>
2025-08-14 18:16:33 +0200

    KM 1

---
+++ Main/WSSB.py
@@ -0,0 +1,29 @@
+#TODO: Implement WSSB Abrechnung
+#Call Center: CA und TA wrschl Fehler
+# CS-ITM
+# CS-OTM
+# DW-ITM
+# FO-ITM
+# FO-OTM
+# FO-S2S
+# FO-SCL
+# JD-ITM
+# JD-OTM
+# SK-ITM
+# SK-OTM
+# SK-S2S
+# TA-ITM
+# TA-OTM
+# TR-ITM
+# TR-OTM
+# TS-ITM
+# TS-OTM
+#Prämien: 49,00; 79,00
+#Deletion Type: 1-7
+
+
+import pandas
+import numpy as np
+import os
+
+
